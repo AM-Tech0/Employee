5f38a98f052181336764e77b9dc570c2d0af1e69
AM-Tech0 <amanbsdm6794@gmail.com>
2025-09-03 16:52:07 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,11 @@
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -28,7 +28,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/user', userRoutes);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/user", userRoutes);
 
 // ✅ Do not use app.listen() for Vercel
 module.exports = app;
