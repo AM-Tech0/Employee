8fb66a4a97de7febb3b055fff269d5d7329275f3
AM-Tech0 <amanbsdm6794@gmail.com>
2025-09-08 14:03:58 +0530

    hlo

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_BASE_URL = process.env.NODE_ENV === "production"
-  ? "https://employee-backend-fawn.vercel.app"
-  : "http://localhost:5000";
+   "https://employee-backend-fawn.vercel.app";
+  // : "http://localhost:5000";
 
 export default API_BASE_URL;
